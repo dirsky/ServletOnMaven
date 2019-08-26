eefb0d308e78ff275cc822ce30e4d0cf687a838f
Frank <dirsky@126.com>
2019-08-26 15:17:32 +0800

    Initial commit

---
+++ src/main/java/com/frank/ServletAnno.java
@@ -0,0 +1,21 @@
+package com.frank;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ServletAnno", urlPatterns = "/hw")
+public class ServletAnno extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/hello.jsp").forward(request,response);
+    }
+}
---
+++ src/main/java/com/frank/ServletDemo.java
@@ -0,0 +1,23 @@
+package com.frank;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+/**
+ * @author Gozhong Xu
+ */
+public class ServletDemo extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/hello.jsp").forward(request,response);
+    }
+}
